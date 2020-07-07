e817bcb6cf3c51373d4afb4a9315f7a3a51773b0
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-07-07 23:05:00 +0100

    minor fix -- log in text area now avoids keyboard

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,15 @@ void main() => runApp(
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
-        child: Column(
+        height: double.infinity,
+        child: Stack(
           children: [
             Container(
-              height: 350,
+              height: size.height * 0.5,
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/images/background.png'),
@@ -94,93 +96,104 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(30),
-              child: Column(
-                children: [
-                  FadeAnimation(
-                    1.8,
-                    Container(
-                      padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color.fromRGBO(143, 148, 251, .2),
-                            blurRadius: 20.0,
-                            offset: Offset(0, 10),
+            Positioned(
+              bottom: size.height > 650 ? 50.0 : 20,
+              right: size.width * 0.05,
+              child: Center(
+                child: Container(
+                  width: size.width * 0.9,
+                  child: Column(
+                    children: [
+                      FadeAnimation(
+                        1.8,
+                        Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromRGBO(143, 148, 251, .3),
+                                blurRadius: 20.0,
+                                offset: Offset(0, 10),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              border: Border(
-                                bottom: BorderSide(
-                                  color: Colors.grey[100],
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: Colors.grey[200],
+                                    ),
+                                  ),
+                                ),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Email",
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey[500]),
+                                  ),
                                 ),
                               ),
-                            ),
-                            child: TextField(
-                              decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Email",
-                                hintStyle: TextStyle(color: Colors.grey[400]),
+                              Container(
+                                padding: const EdgeInsets.all(8),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Password",
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey[500]),
+                                  ),
+                                ),
                               ),
-                            ),
+                            ],
                           ),
-                          Container(
-                            padding: const EdgeInsets.all(8),
-                            child: TextField(
-                              decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "Password",
-                                hintStyle: TextStyle(color: Colors.grey[400]),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 30),
-                  FadeAnimation(
-                    2,
-                    Container(
-                      height: 50,
-                      width: 300,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(
-                          colors: [
-                            Color.fromRGBO(143, 148, 251, 1),
-                            Color.fromRGBO(143, 148, 251, .6),
-                          ],
                         ),
                       ),
-                      child: Center(
-                        child: Text(
-                          "Login",
+                      SizedBox(height: 30),
+                      FadeAnimation(
+                        2,
+                        Container(
+                          height: 50,
+                          width: 300,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromRGBO(143, 148, 251, 1),
+                                Color.fromRGBO(143, 148, 251, .6),
+                              ],
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      FadeAnimation(
+                        2.2,
+                        Text(
+                          "Forgot Password?",
                           style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20,),
+                            color: Color.fromRGBO(143, 148, 251, 1),
+                          ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                  SizedBox(height: 30),
-                  FadeAnimation(
-                    2.2,
-                    Text(
-                      "Forgot Password?",
-                      style: TextStyle(
-                        color: Color.fromRGBO(143, 148, 251, 1),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
           ],
